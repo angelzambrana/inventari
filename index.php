b08f6136d5cefeca9d7f269eba21abd081d03114
Angel Zambrana <angelzambrana@gmail.com>
2015-10-09 22:28:20 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="A little inventory">
+<meta name="author" content="Fundacio Moli Puigvert">
+<link rel="shortcurt icon" href="img/favicon.ico">
+
+<!-- css style ivory  http://weice.in/ivory/ -->
+<link href="css/ivory.css" rel="stylesheet">
+
+<title>INVENTARI</title>
+
+
+</head>
+
+<body>
+
+	<div class="c10 centered first">
+		<div class="alert info">
+			<p style="font-size: 300%; text-align: center;">
+				INVENTARIO
+				</p>
+		</div>
+		<!-- div alert info -->
+
+		<p style="font-size: 200%; text-align: center;">INSERTAR ARTICULOS</P>
+		<p style="font-size: 200%; text-align: center;">CODIGO BARRAS</p>
+		<a href="insert.php"><button type="button"
+				style="margin-left: auto; margin-right: auto; display: block; font-size: 200%; height: 60px;"
+				class="blue">INSERTAR</button> </a>
+		<p style="font-size: 200%; text-align: center;">-----------</p>
+		<p style="font-size: 200%; text-align: center;">LISTAR ARTICULOS</p>
+		<p style="font-size: 200%; text-align: center;">ZONAS</p>
+		<a href="list.php"><button type="button"
+				style="margin-left: auto; margin-right: auto; display: block; font-size: 200%; height: 60px;"
+				class="blue">LISTAR</button> </a>
+		<p style="font-size: 200%; text-align: center;">-----------</p>
+		<p style="font-size: 200%; text-align: center;">INSERTAR ARTICULOS</p>
+		<p style="font-size: 200%; text-align: center;">NUEVOS CODIGO BARRAS</p>
+		<a href="new.php"><button type="button"
+				style="margin-left: auto; margin-right: auto; display: block; font-size: 200%; height: 60px;"
+				class="blue">NUEVO</button> </a>
+	</div>
+	<!-- Div centered -->
+</body>
+</html>
